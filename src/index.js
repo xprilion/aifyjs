19f2a9d44b1d0d24d74c0494ac68767781b4ce83
xprilion <xprilion@gmail.com>
2020-02-01 16:29:00 +0530

    abc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // var $ = require("jquery");
-import "./index.css";
+// import "./index.css";
 
 var baseUrl = "https://568bd7cf.ngrok.io/api";
 // var baseUrl = "https://8b5766f9.ngrok.io/api";
@@ -7,41 +7,84 @@ class aify {
   constructor() {
     var $ = window.$;
     var jQuery = window.jQuery;
+
     this.html = $("html").html();
 
-    // var script = document.createElement('script');
-    // script.src = something;
-    // document.head.appendChild(script);
+    this.linksList = [];
+    this.imageList = [];
+    this.fieldList = [];
 
-    $(document).bind("tripleclick", function() {
-      window.SpeechRecognition =
-        window.webkitSpeechRecognition || window.SpeechRecognition;
-      var finalTranscript = "";
-      var recognition = new window.SpeechRecognition();
-      recognition.interimResults = false;
-      recognition.maxAlternatives = 3;
-      recognition.continuous = true;
-      recognition.onresult = event => {
-        let interimTranscript = "";
-        for (
-          let i = event.resultIndex, len = event.results.length;
-          i < len;
-          i++
-        ) {
-          let transcript = event.results[i][0].transcript;
-          if (event.results[i].isFinal) {
-            finalTranscript += transcript;
-          } else {
-            interimTranscript += transcript;
-          }
+    this.pageSummary = "";
+
+    var self = this;
+
+    var voiceActive = false;
+
+    var dblClickCount = 0;
+
+    window.SpeechRecognition =
+      window.webkitSpeechRecognition || window.SpeechRecognition;
+    var finalTranscript = "";
+    var recognition = new window.SpeechRecognition();
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 3;
+    recognition.continuous = true;
+    recognition.onresult = event => {
+      let interimTranscript = "";
+      for (
+        let i = event.resultIndex, len = event.results.length;
+        i < len;
+        i++
+      ) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          interimTranscript += transcript;
         }
-        console.log(finalTranscript);
+      }
+      console.log(finalTranscript);
+      this.goDialogFlow(finalTranscript);
 
-        finalTranscript = "";
+      finalTranscript = "";
 
+      recognition.stop();
+      voiceActive = false;
+    };
+
+    $("body").dblclick(function() {
+      dblClickCount++;
+      if (dblClickCount > 1) {
+        console.log("Voice input started");
         recognition.stop();
-      };
-      recognition.start();
+        self.speak("Speak command: ");
+        recognition.start();
+        voiceActive = true;
+      } else {
+        console.log("One more tap to activate chatbot...");
+      }
+    });
+
+    var maxInterval = 500;
+    var lastTime = 0;
+    var keyCounter = 0;
+
+    $("body").keydown(function(e) {
+      if (e.which == 70) {
+        keyCounter++;
+        var curTime = new Date().getTime();
+        if (curTime - lastTime < maxInterval && keyCounter == 3) {
+          lastTime = 0;
+          keyCounter = 0;
+          console.log("Voice input started");
+          recognition.stop();
+          self.speak("Speak command: ");
+          recognition.start();
+          voiceActive = true;
+        } else {
+          lastTime = curTime;
+        }
+      }
     });
 
     $.ajax({
@@ -83,6 +126,12 @@ class aify {
               null
             );
             try {
+              var previousValue = element.singleNodeValue.getAttribute(key);
+
+              if (previousValue && previousValue.length > 0) {
+                changes[i].changes = previousValue;
+              }
+
               element.singleNodeValue.setAttribute(key, changeItems[key]);
               element.singleNodeValue.setAttribute(
                 "change",
@@ -134,6 +183,15 @@ class aify {
             );
 
             try {
+              var previousValue = element.singleNodeValue.getAttribute(key);
+
+              if (previousValue && previousValue.length > 0) {
+                changes[i].changes = previousValue;
+              }
+
+              var captionText = "This image contains - ";
+              var ocrText = "Text in this image contains - ";
+
               element.singleNodeValue.setAttribute(key, changeItems[key]);
 
               element.singleNodeValue.setAttribute(
@@ -209,9 +267,101 @@ class aify {
     }
   };
 
-  chatInit = function() {
-    var chatButtonSrc = "<div id='voice_active_banner'>aify</div>";
-    $("body").append(chatButtonSrc);
+  speak = function(text) {
+    console.log(text);
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+  };
+
+  updateLinksList = function() {
+    var self = this;
+    // var linklist = self.linksList;
+
+    var links = document.links;
+    for (var i = 0; i < links.length; i++) {
+      links[i].setAttribute("linkid", i);
+      console.log(links[i]);
+      self.linksList.push(links[i]);
+    }
+  };
+
+  getPageLinks = function() {
+    var self = this;
+    var linklist = self.linksList;
+
+    self.speak("The links on this page are - ");
+
+    for (var i = 0; i < linklist.length; i++) {
+      self.speak("Link number " + i + " - " + linklist[i].href);
+    }
+  };
+
+  followLink = function(linkId) {
+    var self = this;
+    self.speak("Opening link number " + linkId);
+    var element = document.querySelector("[linkid='" + linkId + "']").click();
+  };
+
+  updateImageList = function() {
+    var self = this;
+    var images = document.images;
+    for (var i = 0; i < images.length; i++) {
+      images[i].setAttribute("imgid", i);
+      console.log(images[i]);
+      self.imageList.push(images[i]);
+    }
+  };
+
+  getImages = function() {
+    var self = this;
+    var imglist = self.imageList;
+
+    self.speak("The images on this page are - ");
+
+    for (var i = 0; i < imglist.length; i++) {
+      self.speak("Image number " + i + " - " + imglist[i].alt);
+    }
+  };
+
+  goDialogFlow = function(text) {
+    var self = this;
+    $.ajax({
+      type: "POST",
+      url: "https://aifybot-jshimk.gateway.dialogflow.cloud.ushakov.co",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify({
+        session: "test",
+        queryInput: {
+          text: {
+            text: text,
+            languageCode: "en"
+          }
+        }
+      }),
+      success: function(data) {
+        var res = data.queryResult.intent.displayName;
+        // speechSynthesis.speak(new SpeechSynthesisUtterance(res));
+        switch (res) {
+          case "PageLinks":
+            self.getPageLinks();
+            break;
+
+          case "FollowLink":
+            var linkid = data.queryResult.parameters.linkId;
+            self.followLink(linkid);
+            break;
+
+          case "PageImages":
+            self.getImages();
+            break;
+
+          default:
+        }
+      },
+      error: function() {
+        console.log("Internal Server Error");
+      }
+    });
   };
 }
 
